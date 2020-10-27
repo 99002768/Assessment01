1348cb011f42cde4952dde92024fc82f61f51226
99002768 <72514434+99002768@users.noreply.github.com>
2020-10-27 18:56:50 +0530

    Update Student.java

--- StudentApp/src/main/java/com/studentapp/model/Student.java
+++ StudentApp/src/main/java/com/studentapp/model/Student.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "stdent")
+@Document(collection = "student")
 @TypeAlias("Student")
 
 public class Student {
