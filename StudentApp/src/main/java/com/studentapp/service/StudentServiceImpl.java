e761db4e4d817eee791bbbad8891ac3d29f333e0
99002768 <72514434+99002768@users.noreply.github.com>
2020-10-28 10:07:59 +0530

    Update StudentServiceImpl.java

--- StudentApp/src/main/java/com/studentapp/service/StudentServiceImpl.java
+++ StudentApp/src/main/java/com/studentapp/service/StudentServiceImpl.java
@@ -1,19 +1,22 @@
 package com.studentapp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import com.studentapp.dao.StudentRepository;
 import com.studentapp.exception.StudentNotFoundException;
+
 import com.studentapp.model.Student;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 	
-    @Autowired
+	@Autowired
 	StudentRepository studentRepository;
-    
-    
+
 	@Override
 	public Student addStudent(Student student) {
 		Student newStudent = studentRepository.save(student);
@@ -21,15 +24,37 @@ public class StudentServiceImpl implements StudentService {
 	}
 
 	@Override
-	public Student getStudentById(int id) {
-		return studentRepository.findById(id)
-				.orElseThrow(()-> new StudentNotFoundException("Id not available") );
+	public Student getStudentById(int studentId) throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return studentRepository.findById(studentId)
+				.orElseThrow(()-> new StudentNotFoundException("Id not available"));
 	}
 
 	@Override
-	public boolean deleteStudent(int id) {
-		studentRepository.deleteById(id);
+	public boolean deleteStudent(int studentId) throws StudentNotFoundException {
+		studentRepository.deleteById(studentId);
 		return true;
 	}
+
+	@Override
+	public List<Student> getStudentsByCity(String city) throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return studentRepository.findByAddressCity(city);
+	}
+
+	@Override
+	public List<Student> getStudentsByDept(String dept)throws StudentNotFoundException {
+		// TODO Auto-generated method stub
+		return studentRepository.findByDept(dept);
+	}
+
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
+
+	
+
 
 }
