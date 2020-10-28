cdafb06abf404c85270831b3229466d97cbfe753
99002768 <72514434+99002768@users.noreply.github.com>
2020-10-28 10:08:48 +0530

    Update GlobalExceptionHandler.java

--- StudentApp/src/main/java/com/studentapp/exception/GlobalExceptionHandler.java
+++ StudentApp/src/main/java/com/studentapp/exception/GlobalExceptionHandler.java
@@ -15,90 +15,84 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-	@Override
-	protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		// TODO Auto-generated method stub
-		return super.handleMissingServletRequestParameter(ex, headers, status, request);
-	}
-
-	@Override
-	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, 
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		String message=ex.getMessage();
-		LocalDateTime timestamp=LocalDateTime.now();
-		List<String> details=Arrays.asList("mismatch","not supported");
-		APIErrors errors=new APIErrors(message, timestamp, status, details);
-		return ResponseEntity.status(status).body(errors);
-	}
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@Override
 	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		String message=ex.getMessage();
-		LocalDateTime timestamp=LocalDateTime.now();
-		List<String> details=Arrays.asList("wrong method name","not supported");
-		APIErrors errors=new APIErrors(message, timestamp, status, details);
-		return ResponseEntity.status(status).body(errors);
+		// TODO Auto-generated method stub
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList("wrong method name","not supported");
+		APIErrors errors = new APIErrors(message, timeStamp, status, details);
+		return  ResponseEntity.status(status).body(errors);
 	}
 
 	@Override
 	protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		String message=ex.getMessage();
-		LocalDateTime timestamp=LocalDateTime.now();
-		List<String> details=Arrays.asList("wrong media type","not supported");
-		APIErrors errors=new APIErrors(message, timestamp, status, details);
-		return ResponseEntity.status(status).body(errors);
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList("wrong media type","not supported");
+		APIErrors errors = new APIErrors(message, timeStamp, status, details);
+		return  ResponseEntity.status(status).body(errors);
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		String message=ex.getMessage();
-		LocalDateTime timestamp=LocalDateTime.now();
-		List<String> details=Arrays.asList("wrong path name","not supported");
-		APIErrors errors=new APIErrors(message, timestamp, status, details);
-		return ResponseEntity.status(status).body(errors);
-			
+	protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList("missing path variable name","missing path variable");
+		APIErrors errors = new APIErrors(message, timeStamp, status, details);
+		return  ResponseEntity.status(status).body(errors);
 	}
 
 	@Override
-	protected ResponseEntity<Object> handleMissingServletRequestPart(MissingServletRequestPartException ex,
+	protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		// TODO Auto-generated method stub
-		return super.handleMissingServletRequestPart(ex, headers, status, request);
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList("No request param","missing param ");
+		APIErrors errors = new APIErrors(message, timeStamp, status, details);
+		return  ResponseEntity.status(status).body(errors);
 	}
-	
+
+	@Override
+	protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList("type miss match","mismatch type ");
+		APIErrors errors = new APIErrors(message, timeStamp, status, details);
+		return  ResponseEntity.status(status).body(errors);
+	}
+    
 	@ExceptionHandler(StudentNotFoundException.class)
-	protected ResponseEntity<Object> handlerBookException(StudentNotFoundException ex){
-		String message=ex.getMessage();
-		LocalDateTime timestamp=LocalDateTime.now();
-		List<String> details=Arrays.asList("student is not available");
-		APIErrors errors=new APIErrors(message,timestamp,HttpStatus.BAD_REQUEST,details);
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
-		
+	protected ResponseEntity<Object>  handleStudentException(StudentNotFoundException ex){
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList("Student is Not available");
+		APIErrors errors = new APIErrors(message, timeStamp, HttpStatus.BAD_REQUEST, details);
+		return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
-	
 	@ExceptionHandler(Exception.class)
-	protected ResponseEntity<Object> handlerotherException(Exception ex){
-		String message=ex.getMessage();
-		LocalDateTime timestamp=LocalDateTime.now();
-		List<String> details=Arrays.asList(message);
-		APIErrors errors=new APIErrors(message,timestamp,HttpStatus.INTERNAL_SERVER_ERROR,details);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errors);
-		
+	protected ResponseEntity<Object>  handleOtherException(Exception ex){
+		String message = ex.getMessage();
+		LocalDateTime timeStamp = LocalDateTime.now();
+		List<String> details = 
+				 Arrays.asList(message);
+		APIErrors errors = new APIErrors(message, timeStamp, HttpStatus.INTERNAL_SERVER_ERROR, details);
+		return  ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR).body(errors);
 	}
 	
-	public GlobalExceptionHandler() {
-		
-	}
-
 }
-
